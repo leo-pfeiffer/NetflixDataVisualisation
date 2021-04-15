6af3146e5580f790e5cee3ce0a6787e842f748aa
GlenysAbigail <54170084+GlenysAbigail@users.noreply.github.com>
2021-04-15 16:10:47 +0100

    added transition for bars (only) and fixed missing data issue

--- js/bar.js
+++ js/bar.js
@@ -4,99 +4,32 @@ const width = 800;
 const height = 600;
 const margin = 50;
 
-// const makeVis = function () {
+//creating SVG
+let svg = d3.select("#vis1")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
-//     d3.csv(dataPath)
-//         .then(function (data) {
+//creating scales
+var xScale = d3.scaleBand()
+    .range([margin, width - 30])
+    .padding(0.2);
+var yScale = d3.scaleLinear()
+    .range([height - margin, 0]);
 
-//             const groupedData = d3.nest()
-//                 .key((d) => {
-//                     return d.type;
-//                 })
-//                 .key((d) => {
-//                     return new Date(d.date_added);
-//                 })
-//                 .rollup((v) => {
-//                     return v.length;
-//                 })
-//                 .entries(data);
+//appending X axis
+let xAxis = svg.append("g")
+    .attr("transform", "translate(0, " + (height - margin) + ")")
+    .call(d3.axisBottom(xScale))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-0.8em")
+    .attr("dy", "-0.8em")
+    .attr("transform", "rotate(-90)");
 
-//             const yearExtent = d3.extent(data, (d) => {
-//                 return new Date(d.date_added);
-//             })
-
-//             const xScale = d3.scaleTime().domain(yearExtent).range([0, width])
-
-//             const valueCount = new Array();
-//             groupedData.forEach((d) => {
-//                 (d.values).forEach((e) => {
-//                     if (isNaN(e.value)) console.log('Oopsie', e.value)
-//                     valueCount.push(e.value);
-//                 })
-//             })
-
-//             const countExtent = d3.extent(valueCount, (d) => { return parseInt(d) })
-//             const yScale = d3.scaleLinear().domain(countExtent).range([height, 0]);
-
-//             const xAxis = d3.axisBottom(xScale);
-//             const yAxis = d3.axisLeft(yScale);
-
-//             let svg = d3.select("#vis1")        //creates an SVG element in the body
-//                 .append("svg")
-//                 .attr("width", width + margin)
-//                 .attr("height", height + margin);
-
-//             svg.append("g")
-//                 .attr("class", "x axis")
-//                 .attr("transform", "translate(" + margin + ", " + height + ")")
-//                 .call(xAxis.tickFormat(d3.timeFormat("%y-%m-%d")))
-//                 .selectAll("text")
-//                 .style("text-anchor", "end")
-//                 .attr("dx", "-0.8em")
-//                 .attr("dy", "0.15em")
-//                 .attr("transform", "rotate(-65)");
-
-//             svg.append("g")
-//                 .attr("class", "y axis")
-//                 .attr("transform", "translate(" + margin + ", " + 0 + ")")
-//                 .call(yAxis)
-
-//             const lineGenerator = d3.line()
-//                 .x(function (d) {
-//                     return margin + xScale(new Date(d.key));
-//                 })
-//                 .y(function (d) {
-//                     return yScale(parseInt(d.value));
-//                 })
-
-//             const shows = groupedData[0];
-
-//             shows.values = shows.values.sort(function (a, b) {
-//                 return new Date(a.key) - new Date(b.key);
-//             });
-
-//             const movies = groupedData[1];
-
-//             movies.values = movies.values.sort(function (a, b) {
-//                 return new Date(a.key) - new Date(b.key);
-//             });
-
-//             // console.log(JSON.stringify(shows))
-//             // console.log(JSON.stringify(movies))
-
-//             svg.append("path")
-//                 .datum(shows.values)
-//                 .attr("class", "line shows")
-//                 .attr("d", lineGenerator);
-
-//             svg.append("path")
-//                 .datum(movies.values)
-//                 .attr("class", "line movies")
-//                 .attr("d", lineGenerator);
-//         })
-// }
-
-//makeVis();
+//appending Y axis
+let yAxis = svg.append("g")
+    .attr("transform", "translate(" + margin + ",0)")
 
 const rowConverter = function (d) {
     //function to split the year from the date in date_added column
@@ -123,10 +56,19 @@ const makeVisBar = function (type) {
                         obj.hasOwnProperty(el.date_added) ? obj[el.date_added] += 1 : obj[el.date_added] = 1;
                         return obj
                     }, {})
-
-                // for year in [2008, 2009, ...]: if totalTV has own property `year`, do nothing else set totalTV[year] = 0
+                let years = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+                let addMissingValues = value => {
+                    if (totalTV.hasOwnProperty(value)) {
+                        console.log(value)
+                    }
+                    else {
+                        totalTV[value] = 0
+                    }
+                }
+                years.forEach(addMissingValues)
 
                 return totalTV
+
             }
 
             //function to calculate number of movies added over the years
@@ -137,7 +79,16 @@ const makeVisBar = function (type) {
                         obj.hasOwnProperty(el.date_added) ? obj[el.date_added] += 1 : obj[el.date_added] = 1;
                         return obj
                     }, {})
-
+                let years = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+                let addMissingValues = value => {
+                    if (totalMovie.hasOwnProperty(value)) {
+                        console.log(value)
+                    }
+                    else {
+                        totalMovie[value] = 0
+                    }
+                }
+                years.forEach(addMissingValues)
                 return totalMovie
             }
 
@@ -161,22 +112,14 @@ const makeVisBar = function (type) {
             console.log(keys)
             console.log(values)
 
-            //creating SVG
-            let svg = d3.select("#vis1")
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height)
-
-            //creating scales
-            var xScale = d3.scaleBand()
-                .range([margin, width - 30])
-                .padding(0.2);
-            var yScale = d3.scaleLinear()
-                .range([height - margin, 0]);
-
             xScale.domain([2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]);
             yScale.domain([0, d3.max(values, function (d) { return d; })]);
 
+            // Update the X axis
+            xAxis.call(d3.axisBottom(xScale))
+
+            // Update the Y axis
+            yAxis.transition().duration(1000).call(d3.axisLeft(yScale));
 
             //creating bars
             svg.selectAll(".bar")
@@ -184,15 +127,25 @@ const makeVisBar = function (type) {
                 .enter()
                 .append("rect")
                 .attr("class", "bar")
-                .transition()
-                .duration(500)
+                // .transition()
+                // .duration(500)
                 .attr("x", function (d, i) {
                     return i * 50 + 60;
                 })
                 .attr("width", xScale.bandwidth())
+                //.attr("y", function (d) { return yScale(d); })
+                .attr("y", function (d) { return yScale(0); })
+                .attr("height", function (d) { return height - margin - yScale(0); })
+                .attr("fill", "#E50914");
+
+            //adding animation for each bar
+            svg.selectAll("rect")
+                .data(values)
+                .transition()
+                .duration(800)
                 .attr("y", function (d) { return yScale(d); })
                 .attr("height", function (d) { return height - margin - yScale(d); })
-                .attr("fill", "#E50914");
+                .delay(function (d, i) { console.log(i); return (i * 100) })
 
             //tooltip details
             let tooltip = d3.select('body')
@@ -207,7 +160,7 @@ const makeVisBar = function (type) {
                 .style('color', '#fff')
                 .text('a simple tooltip');
 
-            
+
             //adding tooltips
             svg.selectAll('rect')
                 .data(values)
@@ -229,20 +182,6 @@ const makeVisBar = function (type) {
                     tooltip.text(``).style('visibility', 'hidden');
                 })
 
-            //appending X axis
-            svg.append("g")
-                .attr("transform", "translate(0, " + (height - margin) + ")")
-                .call(d3.axisBottom(xScale))
-                .selectAll("text")
-                .style("text-anchor", "end")
-                .attr("dx", "-0.8em")
-                .attr("dy", "-0.8em")
-                .attr("transform", "rotate(-90)");
-
-            //appending Y axis
-            svg.append("g")
-                .attr("transform", "translate(" + margin + ",0)")
-                .call(d3.axisLeft(yScale));
 
         })
 }
@@ -259,7 +198,6 @@ let showType = "Movie"
 
 toggleButton.onclick = () => {
     showType = showType === "Movie" ? "TV Show" : "Movie"
-    document.getElementById('vis1').getElementsByTagName('svg')[0].remove()
     makeVisBar(showType);
 }
 
